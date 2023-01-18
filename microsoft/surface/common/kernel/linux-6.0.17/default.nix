bf94e84b78d4e20109462a353ba804aeb4477647
Matt Leon <ml@mattleon.com>
2023-01-18 09:01:16 -0500

    Separate config options to different file

--- microsoft/surface/common/kernel/linux-6.0.17/default.nix
+++ microsoft/surface/common/kernel/linux-6.0.17/default.nix
@@ -12,12 +12,14 @@ let
   extraMeta.branch = "6.0";
   patchDir = repos.linux-surface + "/patches/${extraMeta.branch}";
   kernelPatches = pkgs.callPackage ./patches.nix {
-    inherit (lib) kernel;
-    inherit version patchDir;
+    inherit patchDir;
+  };
+  structuredExtraConfig = pkgs.callPackage ./config.nix {
+    inherit lib;
   };
 
   kernelPackages = linuxPackage {
-    inherit version extraMeta kernelPatches;
+    inherit version extraMeta kernelPatches structuredExtraConfig;
     src = fetchurl {
       url = "mirror://kernel/linux/kernel/v6.x/linux-${version}.tar.xz";
       sha256 = "sha256-p+6SCSpUWbtGq/C1RJpOV+i3klkaxOesBO0lQtLOHQg=";
